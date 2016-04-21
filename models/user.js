12436f655bac46946ba4628bb4010d2f141118bd
Tomke Jósiak <tomek.josiak@gmail.com>
2016-04-21 11:03:32 +0200

    Adding password comparing mechanism to user model

--- models/user.js
+++ models/user.js
@@ -22,5 +22,13 @@ userSchema.pre('save', function (next) {
   })
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+
+    callback(null, isMatch);
+  });
+}
+
 export default mongoose.model('user', userSchema);
 
