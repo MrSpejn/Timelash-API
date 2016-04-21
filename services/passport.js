3cb3248b1a8b0ffc2f3dc5dedcc7342d2a1a3440
Tomke Jósiak <tomek.josiak@gmail.com>
2016-04-21 11:04:00 +0200

    Adding email and password strategy to passport

--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,33 @@
-import passport     from 'passport';
-import passport_jwt from 'passport-jwt'
+import passport      from 'passport';
+import passport_jwt  from 'passport-jwt';
+import LocalStrategy from 'passport-local';
 
-import config       from '../config';
-import User         from '../models/user';
+import config        from '../config';
+import User          from '../models/user';
 
 const StrategyJWT = passport_jwt.Strategy;
 const ExtractJWT  = passport_jwt.ExtractJwt;
+
+
+const passwordOptions = {
+  usernameField: 'email'
+};
+
+const passwordLogin = new LocalStrategy(passwordOptions, function(email, password, done) {
+  User.findOne({email}, (err, user) => {
+    if (err)   return done(err, false);
+    if (!user) return done(null, false);
+
+    user.comparePassword(password, (err, isMatch) => {
+      if (err) return done(err, false);
+      if (!isMatch) return done(null, false);
+
+      return done(null, user);
+    });
+
+  })
+});
+
 
 const JWTOptions = {
   jwtFromRequest: ExtractJWT.fromHeader('authorization'),
@@ -25,3 +47,4 @@ const JWTLogin = new StrategyJWT(JWTOptions, (payload, done) => {
 });
 
 passport.use(JWTLogin);
+passport.use(passwordLogin);
